3f877c2b5ac8974194c8dda82d5df6c7a1ec714e
Victor Milhomem <victormllosada@gmail.com>
2024-11-20 19:43:12 -0300

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/VictorMilhomem/goviz
+
+go 1.22.4
---
+++ src/lexer/tokens.go
@@ -0,0 +1,46 @@
+package lexer
+
+import "regexp"
+
+type TokenKind uint8
+
+type Token struct {
+	kind TokenKind
+	text string
+}
+
+const (
+	EOF TokenKind = iota
+	ID
+	INT
+	FLOAT
+
+	OPEN_BRACE
+	CLOSE_BRACE
+	OPEN_BRACKET
+	CLOSE_BRACKET
+
+	// PUNCTS
+	SEMICOLON
+	COMMA
+	COLON
+
+	MINUS
+
+	// KEYWORD
+	STRICT
+	GRAPH
+	SUBGRAPH
+)
+
+var KEYWORDS = map[string]TokenKind{
+	"strict":   STRICT,
+	"graph":    GRAPH,
+	"subgraph": SUBGRAPH,
+}
+
+var PATTERNS = map[TokenKind]*regexp.Regexp{
+	INT:   regexp.MustCompile(`\d+`),
+	FLOAT: regexp.MustCompile(`\d+\.\d+`),
+	ID:    regexp.MustCompile(`[a-zA-Z_][a-zA-Z0-9_]*`),
+}
